4511604ee9be2477ee4736df477f818b3feeb0af
Dennis Lassell <dennis.lassell@gmail.com>
2025-06-14 16:37:27 -0400

    Adding in route connections to next stops

--- MbtaService/repositories/MbtaRepository.cs
+++ MbtaService/repositories/MbtaRepository.cs
@@ -1,34 +1,37 @@
 
 public class MbtaRepository : IMbtaRepository
 {
-    public List<Route> GetRoutesAsync(string routeId)
+    public Task<List<Route>> GetRoutesAsync(string routeId)
     {
-        return new List<Route>()
+        var routeList = new List<Route>()
         {
             new Route() { Id = "First" },
             new Route() { Id = "Second" }
         };
+
+        return Task.FromResult(routeList);
     }
 
-    public List<Stop> GetStopsAsync(string routeId)
+    public Task<List<Stop>> GetStopsAsync(string routeId)
     {
+        var stopList = new List<Stop>();
         if (routeId == "First")
         {
-            return new List<Stop>()
+            stopList = new List<Stop>()
             {
-                new Stop() { Id = "First_Stop_1", attributes = new Attributes() { latitude = 0.0, longitude=0.0} },
-                new Stop() { Id = "First_Stop_2", attributes = new Attributes() { latitude = 1.0, longitude=1.0} }
+                new Stop() { Id = "First_Stop_1", Attributes = new Attributes() { Latitude = 0.0, Longitude=0.0} },
+                new Stop() { Id = "First_Stop_2", Attributes = new Attributes() { Latitude = 1.0, Longitude=1.0} }
             };
         }
         else if (routeId == "Second")
         {
-            return new List<Stop>()
+            stopList = new List<Stop>()
             {
-                new Stop() { Id = "Second_Stop_1", attributes = new Attributes() { latitude = 0.0, longitude=0.0} },
-                new Stop() { Id = "Second_Stop_2", attributes = new Attributes() { latitude = 1.0, longitude=1.0} }
+                new Stop() { Id = "First_Stop_1", Attributes = new Attributes() { Latitude = 0.0, Longitude=0.0} },
+                new Stop() { Id = "Second_Stop_2", Attributes = new Attributes() { Latitude = 1.0, Longitude=1.0} }
             };
         }
 
-        return new List<Stop>();
+        return Task.FromResult(stopList);
     }
 }
